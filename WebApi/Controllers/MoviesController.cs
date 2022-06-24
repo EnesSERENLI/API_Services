35894d49c4f41cf005e83c85508265acef276e33
EnesSERENLI <enes.serenli@hotmail.com>
2022-06-24 16:44:53 +0300

    We took a database. We created a DTO. We created an apicontroller named Movies. We have listed our movies in it.

---
+++ WebApi/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+using WebApi.Models.DTO;
+
+namespace WebApi.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        ImdbDataEntities db = new ImdbDataEntities();
+
+        // Get: api/movies
+        public IHttpActionResult GetMovies()
+        {
+            var movies = db.Movies.Select(x => new MovieDTO
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Description = x.Description,
+                Year = x.Year,
+                Rate = x.Rating
+            });
+            return Ok(movies);
+        }
+    }
+}
---
+++ WebApi/Models/DTO/MovieDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models.DTO
+{
+    public class MovieDTO //Data Table Objects => Instead of giving every information in the database, we will give the information we want to show thanks to DTOs.
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public double Rate { get; set; }
+    }
+}
