c9289c651d2203290f46a526c1e8234253392cd1
EnesSERENLI <enes.serenli@hotmail.com>
2022-06-24 17:59:18 +0300

    Cors has been added to WebApiConfig.

--- WebApi/App_Start/WebApiConfig.cs
+++ WebApi/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -13,6 +14,9 @@ namespace WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var cors = new EnableCorsAttribute("*","*","*"); //If you get an 'Access-Control-Allow-Origin' error when you request an external api project, you need to install the Microsoft.AspNet.Cors library from Nuget Package Manager to the location of the api project and give it permissions. Like here, we can open to all requests as well as limit them.
+            config.EnableCors(cors);
+
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
