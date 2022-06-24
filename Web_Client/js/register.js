fd1cf3548faf31d110132a0808db2783745f52be
EnesSERENLI <enes.serenli@hotmail.com>
2022-06-24 17:49:45 +0300

    Web Client was created and necessary libraries (bootstrap, jquery) were pulled into it. The register and login login pages have been created. js file has been written for the registration process.

---
+++ Web_Client/js/register.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $("#alert-danger").hide();
+    $("#alert-success").hide(); //Do not show a warning when the form is opened.We will show you at the end of the registration process.
+
+    function registerUser() {
+        $.ajax({
+            method: 'Post', //
+            url: 'https://localhost:44399/api/account/register', //the request will go to this url
+            data: { //data to be sent with the request.
+                Email: $("#email").val(),
+                Password: $("#password").val(),
+                ConfirmPassword: $("#confirmpassword").val()
+            },
+            success: function (data) {
+                $("#alert-success").show();
+                $("#info").html('User success created! You can login.')
+                /*window.location = 'https://localhost:44313/login.html'*/ //We can direct the user directly to the login page.
+            },
+            error: function (data) {
+                $("#alert-danger").show();
+                $("#infoerror").html('Failed to create user')
+            }
+        })
+    }
+
+    $("#btnRegister").click(function () { //It will be activated when the Register button is pressed.
+        registerUser();
+    })
+})
